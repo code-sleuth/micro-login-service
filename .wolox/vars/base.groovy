2ef5b787e97a16c0ce912d29f6c65520adaa403b
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-05-17 14:26:07 +0300

    chore(wolox)

---
+++ .wolox/vars/base.groovy
@@ -0,0 +1,15 @@
+@Library('wolox-ci')
+import com.wolox.*;
+
+def call(ProjectConfiguration projectConfig, def _, def nextClosure) {
+    return { variables ->
+        def timeoutTime = projectConfig.env.TIMEOUT ?: 600 // timeout 10 minutes
+        timeout(time: timeoutTime, unit: 'SECONDS') {
+            withEnv(projectConfig.environment) {
+                wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
+                    nextClosure(variables)
+                }
+            }
+        }
+    }
+}
